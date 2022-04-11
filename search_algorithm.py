564a2cfd385c155b23592c24519056e2635ec143
0123Martina <kanokova02@seznam.cz>
2022-04-11 09:24:53 +0200

    funkce linear_search

--- search_algorithm.py
+++ search_algorithm.py
@@ -12,11 +12,26 @@ def read_data(file_name, key):
     return sequential_data
 
 
+def linear_search(searched_sequence, number):
+    positions = []
+    index = 0
+    count = 0
+    for element in searched_sequence:
+        if element == number:
+            positions.append(index)
+            count = count + 1
+        index = index + 1
+    slovník = {"positions": positions, "count": count}
+    return slovník
+
 def main():
     file_name = "sequential.json"
     key = "unordered_numbers"
+    searched_number = 5
     sequential_data = read_data(file_name, key)
+    slovník = linear_search(sequential_data, searched_number)
     print(sequential_data)
+    print(slovník)
 
 
 if __name__ == '__main__':
